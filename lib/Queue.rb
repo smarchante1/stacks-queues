3f2d0d49d66980ab3cddd608f3f8e353211debf9
CheezItMan <chris@adadevelopersacademy.org>
2019-09-02 13:48:33 -0700

    queue implemented

--- lib/Queue.rb
+++ lib/Queue.rb
@@ -1,31 +1,49 @@
 class Queue
 
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = Array.new(10)
+    @max_size = 10
+    @front = -1
+    @back = -1
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1
+      @front = 0
+      @back = 1
+      @store[@front] = element
+    elsif @front == @back
+      raise ArgumentError, "Queue Full"
+    else
+      @store[@back] = element
+      @back = (@back + 1) % @max_size
+    end
   end
 
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
-  end
-
-  def front
-    raise NotImplementedError, "Not yet implemented"
-  end
-
-  def size
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1
+      raise ArgumentError, "Queue Empty"
+    else  
+      value = @store[@front]
+      @front = (@front + 1) % @max_size
+      if @front == @back # Queue is empty
+        @front = @back = -1 
+      end
+      return value
+    end
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return @front == -1
   end
 
   def to_s
-    return @store.to_s
+    list = []
+    current = @front
+    while current != @back
+      list << @store[current]
+      current = (current + 1) % @max_size
+    end
+    return list.to_s
   end
 end
