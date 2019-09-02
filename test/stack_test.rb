7d8fbc7a42da14df6722137c384d6c2236fdf90b
CheezItMan <chris@adadevelopersacademy.org>
2019-09-02 13:34:02 -0700

    revise stack test

--- test/stack_test.rb
+++ test/stack_test.rb
@@ -10,14 +10,12 @@ describe "Test Stack Implementation" do
   end
 
   it "pushes something onto a empty Stack" do
-    skip
     s = Stack.new
     s.push(10)
     s.to_s.must_equal "[10]"
   end
 
   it "pushes multiple somethings onto a Stack" do
-    skip
     s = Stack.new
     s.push(10)
     s.push(20)
@@ -26,59 +24,25 @@ describe "Test Stack Implementation" do
   end
 
   it "starts the size of a Stack at 0" do
-    skip
     s = Stack.new
-    s.size.must_equal 0
     s.empty?.must_equal true
   end
 
   it "removes something from the stack" do
-    skip
     s = Stack.new
     s.push(5)
     removed = s.pop
     removed.must_equal 5
-    s.size.must_equal 0
     s.empty?.must_equal true
   end
 
   it "removes the right something (LIFO)" do
-    skip
     s = Stack.new
     s.push(5)
     s.push(3)
     s.push(7)
     removed = s.pop
     removed.must_equal 7
-    s.size.must_equal 2
     s.to_s.must_equal "[5, 3]"
   end
-
-  it "properly adjusts the size with pushing and poping" do
-    skip
-    s = Stack.new
-    s.empty?.must_equal true
-    s.push(-1)
-    s.push(-60)
-    s.size.must_equal 2
-    s.empty?.must_equal false
-    s.pop
-    s.size.must_equal 1
-    s.pop
-    s.size.must_equal 0
-    s.empty?.must_equal true
-  end
-
-  it "returns the top element on the stack" do
-    skip
-    s = Stack.new
-    s.push(40)
-    s.push(22)
-    s.top.must_equal 22
-  end
-
-  # Challege Tests you could write yourself:
-  # it "doesn't alter the size when you call top" do
-  # it "raises an error if you try to pop from an empty stack" do
-  # it "raises an error if you try to call top on an empty stack" do
 end
