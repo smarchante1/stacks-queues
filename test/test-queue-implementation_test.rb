d36d43d291b115ed1b181efe38a66bb36a255f05
CheezItMan <chris@adadevelopersacademy.org>
2019-09-02 13:48:16 -0700

    adjusted tests

--- test/test-queue-implementation_test.rb
+++ test/test-queue-implementation_test.rb
@@ -11,14 +11,12 @@ describe "Test Queue Implementation" do
   end
 
   it "adds something to an empty Queue" do
-    skip
     q = Queue.new
     q.enqueue(10)
     q.to_s.must_equal "[10]"
   end
 
   it "adds multiple somethings to a Queue" do
-    skip
     q = Queue.new
     q.enqueue(10)
     q.enqueue(20)
@@ -27,62 +25,45 @@ describe "Test Queue Implementation" do
   end
 
   it "starts the size of a Queue at 0" do
-    skip
     q = Queue.new
-    q.size.must_equal 0
     q.empty?.must_equal true
   end
 
   it "removes something from the Queue" do
-    skip
     q = Queue.new
     q.enqueue(5)
     removed = q.dequeue
     removed.must_equal 5
-    q.size.must_equal 0
     q.empty?.must_equal true
   end
 
   it "removes the right something (LIFO)" do
-    skip
     q = Queue.new
     q.enqueue(5)
     q.enqueue(3)
     q.enqueue(7)
     removed = q.dequeue
     removed.must_equal 5
-    q.size.must_equal 2
     q.to_s.must_equal "[3, 7]"
   end
 
   it "properly adjusts the size with enqueueing and dequeueing" do
-    skip
     q = Queue.new
     q.empty?.must_equal true
     q.enqueue(-1)
     q.enqueue(-60)
-    q.size.must_equal 2
     q.empty?.must_equal false
     q.dequeue
-    q.size.must_equal 1
     q.dequeue
-    q.size.must_equal 0
     q.empty?.must_equal true
   end
 
   it "returns the front element in the Queue" do
-    skip
     q = Queue.new
     q.enqueue(40)
     q.enqueue(22)
-    q.front.must_equal 40
     q.enqueue(3)
     q.dequeue
-    q.front.must_equal 22
+    expect(q.dequeue).must_equal 22
   end
-
-  # Challege Tests you could write yourself:
-  # it "doesn't alter the size when you call front" do
-  # it "raises an error if you try to dequeue from an empty Queue" do
-  # it "raises an error if you try to call front on an empty Queue" do
 end
